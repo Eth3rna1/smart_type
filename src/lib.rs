24a39420ce5713032de601a8e4a9cf98adfb633c
Gabriel Mendieta Hernandez <gmendieta4109@gmail.com>
2024-12-21 18:58:42 -0600

    Initial commit to remote

---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+/*
+    A Rust module to add constraints to types and
+    make it easier to add, subtract, multiply, and divide
+    between smart_types
+*/
+pub mod int;
+pub mod string;
+pub mod boolean;
